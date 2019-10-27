f5f5b5f3c4093c8e90f5f695ebf4e756e7e286a9
Jesse Quijano <jesse@quijano.net>
2019-10-26 22:08:16 -0700

    Add business logic/calculations

--- BusinessLogic/BusinessLogic.cs
+++ BusinessLogic/BusinessLogic.cs
@@ -4,9 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeatherStation.BusinessLogic
+namespace WeatherStation.BL
 {
-    class BusinessLogic
+    public class BusinessLogic : IBusinessLogic
     {
+        public const int MEASUREMENT_INTERVAL = 5;
+
+        public int LowTemperature { get; set; }
+        public int HighTemperature { get; set; }
+        public double WindSpeed { get; set; }
+        public string WeatherCondition { get; set; }
+
+        public List<int> GetWindSpeedValues(int high = 30)
+        {
+            List<int> speeds = new List<int>();
+
+            for (int i=0; i <= high; i += MEASUREMENT_INTERVAL)
+            {
+                speeds.Add(i);
+            }
+
+            return speeds;
+        }
+
+        public List<int> GetLowTemperatureValues(int low = -15, int high = 50)
+        {
+            List<int> temps = new List<int>();
+
+            for (int i = low; i <= high; i += MEASUREMENT_INTERVAL)
+            {
+                temps.Add(i);
+            }
+
+            return temps;
+        }
+
+        public List<int> GetHighTemperatureValues(int low = 50, int high = 100)
+        {
+            List<int> temps = new List<int>();
+
+            for (int i = low; i <= high; i += MEASUREMENT_INTERVAL)
+            {
+                temps.Add(i);
+            }
+
+            return temps;
+        }
+
+        public List<string> GetSkyConditions()
+        {
+            List<string> conditions = new List<string>();
+
+            foreach (string condition in (string[]) Enum.GetNames(typeof(SkyConditions)))
+            {
+                conditions.Add(condition);
+            }
+
+            return conditions;
+        }
+
+        public double CalculateWindChill()
+        {
+            //Wind Chill = 35.74 + 0.6215T – 35.75(V ^ 0.16) + 0.4275T(V ^ 0.16)
+
+            return 35.74 + 0.6215 * this.HighTemperature - 35.75 * (Math.Pow(this.WindSpeed , 0.16)) + 0.4275 * this.HighTemperature * (Math.Pow(this.WindSpeed, 0.16));
+        }
+
+        public string GetSkyImageName(SkyConditions sky)
+        {
+            string fileName;
+            switch (sky)
+            {
+                case SkyConditions.Clear:
+                    fileName = "clear.jpg";
+                    break;
+                case SkyConditions.Cloudy:
+                    fileName = "cloudy.jpg";
+                    break;
+                case SkyConditions.PartlyCloudy:
+                    fileName = "partlycloudy.jpg";
+                    break;
+                case SkyConditions.Rain:
+                case SkyConditions.Snow:
+                    fileName = "rain.jpg";
+                    break;
+                default:
+                    fileName = "clear.jpg";
+                    break;
+            }
+
+            return fileName;
+        }
     }
 }
--- BusinessLogic/IBusinessLogic.cs
+++ BusinessLogic/IBusinessLogic.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeatherStation.BusinessLogic
+namespace WeatherStation.BL
 {
     interface IBusinessLogic
     {
+        int HighTemperature { get; set; }
+        int LowTemperature { get; set; }
+        double WindSpeed { get; set; }
+        string WeatherCondition { get; set; }
+        double CalculateWindChill();
         List<int> GetHighTemperatureValues(int low = 50, int high = 100);
         List<int> GetLowTemperatureValues(int low = -15, int high = 50);
-        List<int> GetWindChillValues();
+        List<int> GetWindSpeedValues(int high = 30);
         List<string> GetSkyConditions();
+        string GetSkyImageName(SkyConditions sky);
     }
 }
