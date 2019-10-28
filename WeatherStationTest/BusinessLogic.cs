5cda5d525dce78a75c60a1908d5054e7df290fa2
Jesse Quijano <jesse@quijano.net>
2019-10-27 17:44:33 -0700

    Add nunit project and unit tests

--- WeatherStationTest/BusinessLogic.cs
+++ WeatherStationTest/BusinessLogic.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using WeatherStation.BL;
 
 namespace WeatherStation.BLTests
@@ -24,13 +25,55 @@ namespace WeatherStation.BLTests
         }
 
         [Test]
-        //[ExpectedMessage="You can't do that!"]
         public void TempTooHighFails()
         {
             BusinessLogic bl = new BusinessLogic();
 
-            bl.HighTemperature = 50;
+            bl.HighTemperature = 70;
             bl.WindSpeed = 20;
+
+            Assert.AreEqual(bl.CalculateWindChill(), 9999);
+        }
+
+        [Test]
+        public void CloudyImageIsCorrect()
+        {
+            BusinessLogic bl = new BusinessLogic();
+            var img = bl.GetSkyImageName(SkyConditions.Cloudy);
+
+            Assert.AreEqual(img, "cloudy.jpg");
+        }
+
+        [Test]
+        public void CorrectNumberOfHighTempOptions()
+        {
+            BusinessLogic bl = new BusinessLogic();
+            var options = bl.GetHighTemperatureValues();
+
+            Assert.AreEqual(options.Count, 11);
+        }
+
+        [Test]
+        public void SkyConditionsCountCorrect()
+        {
+            BusinessLogic bl = new BusinessLogic();
+            var conditions = bl.GetSkyConditions();
+
+            Assert.AreEqual(conditions.Count, 5);
+        }
+
+        [Test]
+        public void SkyConditionOptionsCorrect()
+        {
+            List<string> control = new List<string>
+            {
+                "Clear", "Cloudy", "PartlyCloudy", "Rain", "Snow"
+            };
+
+            BusinessLogic bl = new BusinessLogic();
+            var conditions = bl.GetSkyConditions();
+
+            Assert.AreEqual(conditions, control);
         }
     }
 }
