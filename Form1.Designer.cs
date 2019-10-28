68839d886f4714bccd7a4f7520fe8236dd594ecc
Jesse Quijano <jesse@quijano.net>
2019-10-27 23:15:03 -0700

    implement form using business logic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,37 +29,278 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblWindChill = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblLow = new System.Windows.Forms.Label();
+            this.lblHigh = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pbxSky = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnResults = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbConditions = new System.Windows.Forms.ComboBox();
+            this.cmbSpeeds = new System.Windows.Forms.ComboBox();
+            this.cmbLows = new System.Windows.Forms.ComboBox();
+            this.cmbHighs = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSky)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(669, 72);
+            this.groupBox1.Controls.Add(this.lblWindChill);
+            this.groupBox1.Controls.Add(this.lblSpeed);
+            this.groupBox1.Controls.Add(this.lblLow);
+            this.groupBox1.Controls.Add(this.lblHigh);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.pbxSky);
+            this.groupBox1.Location = new System.Drawing.Point(305, 37);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(404, 518);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(323, 269);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Results";
+            // 
+            // lblWindChill
+            // 
+            this.lblWindChill.AutoSize = true;
+            this.lblWindChill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWindChill.Location = new System.Drawing.Point(190, 203);
+            this.lblWindChill.Name = "lblWindChill";
+            this.lblWindChill.Size = new System.Drawing.Size(0, 20);
+            this.lblWindChill.TabIndex = 9;
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpeed.Location = new System.Drawing.Point(23, 201);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(0, 25);
+            this.lblSpeed.TabIndex = 8;
+            // 
+            // lblLow
+            // 
+            this.lblLow.AutoSize = true;
+            this.lblLow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLow.Location = new System.Drawing.Point(23, 131);
+            this.lblLow.Name = "lblLow";
+            this.lblLow.Size = new System.Drawing.Size(0, 25);
+            this.lblLow.TabIndex = 7;
+            // 
+            // lblHigh
+            // 
+            this.lblHigh.AutoSize = true;
+            this.lblHigh.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHigh.Location = new System.Drawing.Point(23, 58);
+            this.lblHigh.Name = "lblHigh";
+            this.lblHigh.Size = new System.Drawing.Size(0, 25);
+            this.lblHigh.TabIndex = 6;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(163, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 20);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Sky Condition";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(163, 170);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 20);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Wind Chill";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Wind Speed";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Low";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "High";
+            // 
+            // pbxSky
+            // 
+            this.pbxSky.Location = new System.Drawing.Point(180, 33);
+            this.pbxSky.Name = "pbxSky";
+            this.pbxSky.Size = new System.Drawing.Size(68, 68);
+            this.pbxSky.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSky.TabIndex = 0;
+            this.pbxSky.TabStop = false;
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(51, 72);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnResults);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.cmbConditions);
+            this.groupBox2.Controls.Add(this.cmbSpeeds);
+            this.groupBox2.Controls.Add(this.cmbLows);
+            this.groupBox2.Controls.Add(this.cmbHighs);
+            this.groupBox2.Location = new System.Drawing.Point(26, 37);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(592, 518);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(254, 269);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Text = "Weather Input Values";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sky Condition";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Wind Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Predicted\r\nLow Temperature";
+            // 
+            // btnResults
+            // 
+            this.btnResults.Enabled = false;
+            this.btnResults.Location = new System.Drawing.Point(8, 223);
+            this.btnResults.Name = "btnResults";
+            this.btnResults.Size = new System.Drawing.Size(225, 23);
+            this.btnResults.TabIndex = 5;
+            this.btnResults.Text = "Generate Results";
+            this.btnResults.UseVisualStyleBackColor = true;
+            this.btnResults.Click += new System.EventHandler(this.btnResults_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Predicted\r\nHigh Temperature";
+            // 
+            // cmbConditions
+            // 
+            this.cmbConditions.FormattingEnabled = true;
+            this.cmbConditions.Location = new System.Drawing.Point(112, 171);
+            this.cmbConditions.Name = "cmbConditions";
+            this.cmbConditions.Size = new System.Drawing.Size(121, 21);
+            this.cmbConditions.TabIndex = 3;
+            this.cmbConditions.SelectedIndexChanged += new System.EventHandler(this.cmbConditions_SelectedIndexChanged);
+            // 
+            // cmbSpeeds
+            // 
+            this.cmbSpeeds.FormattingEnabled = true;
+            this.cmbSpeeds.Location = new System.Drawing.Point(113, 126);
+            this.cmbSpeeds.Name = "cmbSpeeds";
+            this.cmbSpeeds.Size = new System.Drawing.Size(121, 21);
+            this.cmbSpeeds.TabIndex = 2;
+            this.cmbSpeeds.SelectedIndexChanged += new System.EventHandler(this.cmbSpeeds_SelectedIndexChanged);
+            // 
+            // cmbLows
+            // 
+            this.cmbLows.FormattingEnabled = true;
+            this.cmbLows.Location = new System.Drawing.Point(112, 80);
+            this.cmbLows.Name = "cmbLows";
+            this.cmbLows.Size = new System.Drawing.Size(121, 21);
+            this.cmbLows.TabIndex = 1;
+            this.cmbLows.SelectedIndexChanged += new System.EventHandler(this.cmbLows_SelectedIndexChanged_1);
+            // 
+            // cmbHighs
+            // 
+            this.cmbHighs.FormattingEnabled = true;
+            this.cmbHighs.Location = new System.Drawing.Point(113, 36);
+            this.cmbHighs.Name = "cmbHighs";
+            this.cmbHighs.Size = new System.Drawing.Size(121, 21);
+            this.cmbHighs.TabIndex = 0;
+            this.cmbHighs.SelectedIndexChanged += new System.EventHandler(this.cmbHighs_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(540, 316);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 27);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1126, 650);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(655, 352);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Weather Station";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSky)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +309,26 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbLows;
+        private System.Windows.Forms.ComboBox cmbHighs;
+        private System.Windows.Forms.Button btnResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbConditions;
+        private System.Windows.Forms.ComboBox cmbSpeeds;
+        private System.Windows.Forms.PictureBox pbxSky;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblLow;
+        private System.Windows.Forms.Label lblHigh;
+        private System.Windows.Forms.Label lblWindChill;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
